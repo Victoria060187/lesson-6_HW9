9d241aea42f8626d9b0d54ac93f66d5340e391af
Victoria Laputko <vikalaputko87@gmail.com>
2023-06-11 14:23:01 +0300

    add new folder and files

---
+++ index.js
@@ -0,0 +1,7 @@
+function removeElement(array, item) {
+  return array.filter((element) => element !== item);
+}
+
+const array = [1, 2, 3, 4, 5, 6, 7];
+const newArray = removeElement(array, 5);
+console.log(newArray);
